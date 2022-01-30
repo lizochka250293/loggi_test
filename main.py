d4ff8f47233c6af89f49ab1e32be5af0777bd854
vera <lerika250293@mail.ru>
2022-01-30 21:16:57 +0300

    'cheak'

--- main.py
+++ main.py
@@ -1,45 +1,19 @@
-import logging
+import datetime
+def logger(func):
+    def arguments(arg1, arg2):
+        print(datetime.now())
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+        func()
+    return arguments(arg1, arg2)
+@ logger
+def suma(a, b):
+    if a>0 and b >0:
+        return a+b
+    elif a<0 and b<0:
+        return a-b
+    else:
+        return 0
 
-formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
-
-file_handler = logging.FileHandler('hm.log')
-file_handler.setFormatter(formatter)
-
-logger.addHandler(file_handler)
-def add(a,b):
-    return a + b
-
-
-def subtract(a, b):
-
-        return a - b
-
-
-
-def multiplicator(a, b):
-    return a * b
-
-
-def division(a, b):
-
-    return a / b
-
-
-
-num_1 = 10
-num_2 = 5
-
-add_result = add(num_1, num_2)
-logger.debug(f"Add: {num_1} + {num_2} = {add_result}")
-
-sub_result = subtract(num_1, num_2)
-logger.debug(f"Sub: {num_1} - {num_2} = {sub_result}")
-
-mul_result = multiplicator(num_1, num_2)
-logger.debug(f"Mul: {num_1} * {num_2} = {mul_result}")
-
-div_result = division(num_1, num_2)
-logger.debug(f"Div: {num_1} / {num_2} = {div_result}")
+a = 3
+b = 7
+suma = logger(suma)
